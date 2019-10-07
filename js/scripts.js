551860b62f29ac3bf2133a8a34ce9db24a5e7e08
rashidamuda <rashid.amuda@gmail.com>
2019-10-07 11:30:50 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
+var totalCosts = [];
+
+function PlaceOrder(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 0;
+}
+
+var sizePizza = ["Small", "Medium", "Large"];
+var toppingsPizza = ["Pepperoni", "chicken", "bacon", "beef"];
+
+PlaceOrder.prototype.cost = function() {
+  if (this.size === sizePizza[0]) {
+    this.price += 400;
+  } else if (this.size === sizePizza[1]) {
+    this.price += 700;
+  } else if (this.size === sizePizza[2]) {
+    this.price += 1000;
+  }
+
+  if (this.toppings === toppingsPizza[0]) {
+    this.price += 150;
+  } else if (this.toppings === toppingsPizza[1]) {
+    this.price += 150;
+  } else if (this.toppings === toppingsPizza[2]) {
+    this.price += 150;
+  } else if (this.toppings === toppingsPizza[3]) {
+    this.price += 150;
+  }
+  return this.price;
+};
+
+PlaceOrder.prototype.totalCost = function() {
+  var orderTotal = 0;
+  for (var order = 0; order < totalCosts.length; order++) {
+    orderTotal += totalCosts[order];
+    alert(orderTotal);
+    
+  }
+};
 $("document").ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
@@ -15,6 +55,7 @@ $("document").ready(function() {
       DeliveryOrder.push($(this).val());
       alert("would you like your pizza delivered ? " + DeliveryOrder);
     });
+   
   });
 
   $("form").submit(function(event) {
@@ -30,19 +71,9 @@ $("document").ready(function() {
       .find(":selected")
       .text();
     alert(" number of pizzas ordered .. " + number);
+    var newPizzaOrder = new PlaceOrder(sizePizza, toppingsPizza);
+       newPizzaOrder.cost();
+       totalCosts.push(newPizzaOrder.price);
+       alert(totalCosts)
   });
-  var totalPrice = [];
-  function Order(optionSize) {
-    this.optionSize = optionSize;
-    this.toppings = 150;
-  }
-  order.prototype.pizzaCost = function() {
-    if (this.optionSize === "large") {
-      this.pizzaPrice = 1000;
-    } else if (this.optionSize === "medium") {
-      this.pizzaPrice = 700;
-    } else if (this.optionSize === "small") {
-        this.pizzaPrice = 400;
-    }
-  };
 });
